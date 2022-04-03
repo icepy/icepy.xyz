089785648f7a0a702192a876b087e775975ab58d
icepy <xiangwenwe@foxmail.com>
2022-04-03 18:56:41 +0800

    chore: 重新编辑文字

--- src/components/Content/index.jsx
+++ src/components/Content/index.jsx
@@ -17,20 +17,20 @@ export default class Content extends Component {
         </div>
         <div className="App-Content">
           <p>
-            你好，我是<strong>“wen（五溪）”</strong>。 
+            Hi，我是<strong>wen（五溪）</strong>
           </p>
           <p>  
-            2012年开始从事编程的工作，经历了从前端到大前端变革的整个过程。
+            2012年开始从事编程的工作，经历了前端到大前端变革的整个过程。
           </p>
           <p>
             深入理解 <strong>移动跨平台架构领域</strong> 的设计和原理，
             在 <strong>Web前端，移动Hybrid容器架构，小程序容器架构</strong> 上有着丰富的实践和沉淀，
             其他技术方向上如 Node.js Rust 都有一定的涉猎。
           </p>
-          <p>热衷于 Web3 技术的内容分享，深入理解 <strong>以太坊平台</strong> 的设计与原理，熟练 Solidity ethers.js Graph Chainlink 等开发技术，声明：<strong>并未参与任何区块链项目</strong>。</p>
-          <p>在团队管理方面追求务实，有效沟通，有能力带领团队解决问题，完成目标，落实结果。</p>
+          <p>热衷于 Web3 技术的内容分享，深入理解 <strong>以太坊平台</strong> 的设计与原理，熟练 Solidity ethers.js Graph Chainlink 等开发技术，<strong>声明：并未参与任何区块链项目，挖矿或交易。</strong></p>
+          <p>在团队管理上追求务实，有效沟通，有能力带领团队解决问题，完成目标，落实结果。</p>
           <p>
-            在生活里我喜欢烹饪食物和骑着摩托车自由旅行，如果你也热衷于摩托车旅行，不妨坐下来和我聊聊。
+            在生活中我喜欢烹饪食物，维修摩托车和骑着摩托车旅行，如果你也喜欢骑着摩托车旅行，不妨坐下来和我聊聊。
           </p>
           <ul>
             <li>
@@ -53,10 +53,10 @@ export default class Content extends Component {
             </li>
           </ul>
           <p>
-            目前我在大湘西，从事自己喜欢的工作，认真快乐的生活。
+            目前我在湘西，从事自己喜欢的工作，认真快乐的生活。
           </p>
           <p>
-            最后我想感谢在我人生旅途中，遇见的你们。
+            最后我想感谢，在我人生旅途中遇见的你们。
           </p>
           <p>
             <a
